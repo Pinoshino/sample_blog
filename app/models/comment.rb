28b545f062f25ea3dda6a2c328aa89e8d017799a
Shinohara Yuya <mrdragon613@gmail.com>
2016-04-18 04:05:25 +0000

    update some func

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :blog
+
+    validates :commenter, presence: {message: "投稿者名が空です"}, length: {maximum: 20, message: "投稿者名が長過ぎます"}
+    validates :body, presence: {message: "コメントが空です"}
+
+
 end
